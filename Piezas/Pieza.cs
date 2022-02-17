1835c1378881df292b9c2119fc19844a2ee32dec
Saúl <saulvaldelvira@gmail.com>
2022-02-17 20:48:21 +0100

    Piezas

---
+++ Piezas/Pieza.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Piezas {
+    public enum Tipo {REY, REINA, TORRE, ALFIL, CABALLO, PEON}
+    public enum Color {BLANCA=1, NEGRA=0}
+    public abstract class Pieza {
+        public Color Color { get; private set; }
+        public Tipo Tipo { get; private set; }
+        public Pieza(Color c, Tipo t) {
+            Color = c;
+            Tipo = t;
+        }
+    }
+
+    public class Torre: Pieza {
+        public Torre(Color c): base(c, Tipo.TORRE){
+
+        }
+    }
+}
---
+++ Tablero/Tablero.cs
@@ -0,0 +1,26 @@
+﻿using Piezas;
+
+namespace Tablero {
+    internal struct Numero_Piezas {
+        const int REY = 1;
+        const int REINA = 1;
+        const int TORRE = 2;
+        const int ALFIL = 2;
+        const int CABALLO = 2;
+        const int PEON = 8;
+    }
+    public class Tablero {
+        private Pieza[,] tablero;
+        public Tablero() {
+            tablero = new Pieza[8,8];
+            InicializaTablero();
+        }
+
+        private void InicializaTablero() {
+            for (int i = 0; i < tablero.Length; i++)
+                for (int j = 0; j < tablero.Length; j++)
+                    tablero[i, j] = null;
+            tablero[0, 0] = new Pieza(Color.NEGRA, Tipo.TORRE);
+        }
+    }
+}
